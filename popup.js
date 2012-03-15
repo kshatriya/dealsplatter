3ed7df5b79099f712d54b912ca05abc8d83804cb
Vasudev Gadge <vasudevgadge@yahoo.com>
2012-03-15 00:09:19 -0400

    displaying deals

--- popup.js
+++ popup.js
@@ -6,7 +6,15 @@ function getDeals()
 			type : 'POST',
 			success: function(data) {
 				$('#deals').html('');
-				$('#deals').html(data);	
+				var result = eval('('+data+')');
+				//$('#deals').html(deals);
+				
+				for (var i = 0; i < result.length || i <= 50; i++)
+				{
+					$('#deals').append('<div id="deals_'+i+'">');
+					$('#deals').append('<a href="http://dealsplatter.com/deal/'+result[i].id+'/">'+result[i]['title']);
+					$('#deals').append('</a></div><br/><br/>');
+				}
 			}
 		});
 }
