e230aa48cba0de9ca8f5604421c844edb9213e2c
Vasudev Gadge <vasudevgadge@yahoo.com>
2012-03-15 22:34:26 -0400

    added slim scroll bar , has few bugs

--- popup.js
+++ popup.js
@@ -1,18 +1,22 @@
 function getDeals()
 {
+	$("body").css("overflow", "hidden");
+	
 	var search = $('#search').val();
 	$.ajax({
 			url : 'http://dealsplatter.com/api/search.php?q='+search,
 			type : 'POST',
 			success: function(data) {
 				$('#deals').html('');
+								
 				var result = eval('('+data+')');
-				//$('#deals').html(deals);
-				
+				$('#deals').append('<ul id="deal_list">');			
 				for (var i = 0; i < result.length || i <= 50; i++)
 				{
-					$('#deals').append('<div id="deals_'+i+'"><a href="http://dealsplatter.com/deal/'+result[i].id+'/">'+result[i]['title']+'</a></div><br/><br/>');
+					$('#deal_list').append('<li><a target="_blank" href="http://dealsplatter.com/deal/'+result[i]['id']+'/">'+result[i]['title']+'</a></li>');
 				}
+				//$('#deals').append('</ul>');
 			}
 		});
+//$('#deals').slimscroll({color: '#000',size: '5px'});
 }
